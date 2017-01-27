94e3b90d2f8a3300868b082446cb7813f3dc4bf2
Ailen Sierra <a-sierra@users.noreply.github.com>
2017-01-27 20:17:40 -0300

    Implement fallback lang logic

--- src/utils.js
+++ src/utils.js
@@ -3,13 +3,16 @@ export function getBestMessageTranslation(translations, culture, messageKey) {
 
   if (!messageKey || !culture || !translations) return null;
 
-  //TODO: this is a quick draft, should improve fallback procedure
-  let lang = translations[culture];
+  let lang;
 
-  if (!lang) return null;
+  if (translations[culture])
+    lang = translations[culture];
+  else if (translations[getLangFromLocale(culture)])
+    lang = translations[getLangFromLocale(culture)];
+  else 
+    return null;
 
   return lang[messageKey];
-
 }
 
 export function mergeTranslations(baseTranslations, overrideTranslations) {
@@ -20,3 +23,10 @@ export function mergeTranslations(baseTranslations, overrideTranslations) {
   };
 
 }
+
+function getLangFromLocale(locale) {
+  let re = /^[A-Za-z]{2}/,
+  result = re.exec(locale);
+  return result[0];  
+};
+
